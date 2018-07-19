83ec61bd6951d5575cf1bb3b21b906978e32f434
xuyf <xuyf778@163.com>
2018-07-19 17:13:18 +0800

    第一次提交

---
+++ Basemodule/src/main/java/com/xuyf/basemodule/common/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.xuyf.basemodule.common
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * author：xuyf
+ * email:xuyf778@163.com
+ */
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
