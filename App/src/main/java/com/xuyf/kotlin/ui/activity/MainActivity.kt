a193b5056edcc5e48e7e34b416a8edeca7ea512d
xuyf <xuyf778@163.com>
2018-07-20 13:14:26 +0800

    are you ok

--- App/src/main/java/com/xuyf/kotlin/ui/activity/MainActivity.kt
+++ App/src/main/java/com/xuyf/kotlin/ui/activity/MainActivity.kt
@@ -1,14 +1,14 @@
-package com.xuyf.kotlin
+package com.xuyf.kotlin.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import com.xuyf.kotlin.R
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i(TAG,"jintianhenhao")
+
     }
 }
