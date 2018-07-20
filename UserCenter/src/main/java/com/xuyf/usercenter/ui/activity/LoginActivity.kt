e7c3d9f186f12a3345c5f9e7cb72f29cb2da501d
xuyf <xuyf778@163.com>
2018-07-20 17:02:07 +0800

    添加Anko common包

--- UserCenter/src/main/java/com/xuyf/usercenter/ui/activity/LoginActivity.kt
+++ UserCenter/src/main/java/com/xuyf/usercenter/ui/activity/LoginActivity.kt
@@ -1,31 +1,9 @@
 package com.xuyf.usercenter.ui.activity
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.annotation.TargetApi
-import android.content.pm.PackageManager
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.app.LoaderManager.LoaderCallbacks
-import android.content.CursorLoader
-import android.content.Loader
-import android.database.Cursor
-import android.net.Uri
-import android.os.AsyncTask
-import android.os.Build
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.text.TextUtils
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.ArrayAdapter
-import android.widget.TextView
-
-import java.util.ArrayList
-import android.Manifest.permission.READ_CONTACTS
+import android.support.v7.app.AppCompatActivity
 import com.xuyf.usercenter.R
-
-import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.toast
 
 /**
  * A login screen that offers login via email/password.
@@ -35,7 +13,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        toast("沃日你麻麻批")
     }
 
 }
